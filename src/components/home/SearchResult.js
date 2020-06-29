078d6a91105913893ebfef876a6ca73d4f549bd5
Liam Best <liambbest@googlemail.com>
2020-06-29 18:19:59 +0200

    Search image render fixed

--- src/components/home/SearchResult.js
+++ src/components/home/SearchResult.js
@@ -4,7 +4,13 @@ export default class SearchResult extends Component {
   render() {
     /*conditionally render image depending on result type*/
     let imageUrl = null;
-    if (this.props.result.images) {
+    if (
+      (this.props.result.images === undefined ||
+        this.props.result.images.length === 0) &&
+      this.props.result.type !== "track"
+    ) {
+      imageUrl = "#";
+    } else if (this.props.result.images) {
       imageUrl = this.props.result.images[1].url;
     } else if (this.props.result.type === "track") {
       imageUrl = this.props.result.album.images[1].url;
@@ -18,9 +24,19 @@ export default class SearchResult extends Component {
       type = this.props.result.type;
     }
 
+    /*conditionally render album/track artist*/
+    let objectArtist = null;
+    if (this.props.result.type === "album") {
+      objectArtist = " - " + this.props.result.artists[0].name;
+    } else if (this.props.result.type === "track") {
+      objectArtist = " - " + this.props.result.album.artists[0].name;
+    }
     return (
       <div>
-        <p>{this.props.result.name}</p>
+        <p>
+          {this.props.result.name}
+          {objectArtist}
+        </p>
         <img src={imageUrl} alt="cover"></img>
         <p>{type}</p>
       </div>
