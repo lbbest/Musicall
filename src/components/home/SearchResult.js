9592385c7e74ec9b5f2cfab77e4a80c86f8bed69
Liam Best <liambbest@googlemail.com>
2020-07-01 17:21:40 +0200

    Search result images correctly rendering

--- src/components/home/SearchResult.js
+++ src/components/home/SearchResult.js
@@ -4,41 +4,64 @@ export default class SearchResult extends Component {
   render() {
     /*conditionally render image depending on result type*/
     let imageUrl = null;
-    if (
-      (this.props.result.type === "track" &&
-        (this.props.result.album.images === undefined ||
-          this.props.result.album.images.length === 0)) ||
-      this.props.result.images === undefined ||
-      this.props.result.images.length === 0
-    ) {
-      imageUrl = "https://static.thenounproject.com/png/29993-200.png";
-    } else {
-      imageUrl = this.props.result.images[0].url;
+    if (this.props.result.type === "artist") {
+      if (
+        this.props.result.images === undefined ||
+        this.props.result.images.length === 0
+      ) {
+        imageUrl = "https://static.thenounproject.com/png/29993-200.png";
+      } else {
+        imageUrl = this.props.result.images[0].url;
+      }
+    } else if (this.props.result.type === "album") {
+      if (
+        this.props.result.images === undefined ||
+        this.props.result.images.length === 0
+      ) {
+        imageUrl = "https://static.thenounproject.com/png/29993-200.png";
+      } else {
+        imageUrl = this.props.result.images[0].url;
+      }
+    } else if (this.props.result.type === "track") {
+      if (
+        this.props.result.album === undefined ||
+        this.props.result.album.images === undefined ||
+        this.props.result.album.images.length === 0
+      ) {
+        imageUrl = "https://static.thenounproject.com/png/29993-200.png";
+      } else {
+        imageUrl = this.props.result.album.images[0].url;
+      }
     }
 
     /*conditionally render album type if result is album*/
     let type = null;
     if (this.props.result.type === "album") {
       type = this.props.result.album_type;
-    } else {
+    } else if (this.props.result.type === "track") {
       type = this.props.result.type;
     }
 
     /*conditionally render album/track artist*/
     let objectArtist = null;
     if (this.props.result.type === "album") {
-      objectArtist = " - " + this.props.result.artists[0].name;
+      objectArtist = this.props.result.artists[0].name;
     } else if (this.props.result.type === "track") {
-      objectArtist = " - " + this.props.result.album.artists[0].name;
+      objectArtist = this.props.result.album.artists[0].name;
+    } else if (this.props.result.type === "artist") {
+      objectArtist = this.props.result.type;
     }
+
     return (
-      <div>
-        <p>
-          {this.props.result.name}
-          {objectArtist}
-        </p>
-        <img src={imageUrl} alt="cover"></img>
-        <p>{type}</p>
+      <div className="search-result">
+        <img src={imageUrl} alt="cover" className="search-result-img"></img>
+        <div className="search-result-info">
+          <div>
+            <p className="search-result-title">{this.props.result.name}</p>
+            <p className="search-result-subtitle">{objectArtist}</p>
+          </div>
+          <p className="search-result-type">{type}</p>
+        </div>
       </div>
     );
   }
