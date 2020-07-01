57e22dcca5a092ededaa016c6dfc341bb022aa78
Liam Best <liambbest@googlemail.com>
2020-07-01 17:26:33 +0200

    Nav link mock-ups updated

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -6,8 +6,9 @@ export default class Nav extends Component {
       <nav>
         <p id="nav-logo">music.all</p>
         <ul className="nav-link-container">
-          <li className="nav-link">About</li>
-          <li className="nav-link">Contact</li>
+          <li className="nav-link">Featured</li>
+          <li className="nav-link">New Releases</li>
+          <li className="nav-link">Blog</li>
         </ul>
       </nav>
     );
