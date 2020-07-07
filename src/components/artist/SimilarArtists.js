f3586264e67a343549f0af1f97d596f032c8b7fb
Liam Best <liambbest@googlemail.com>
2020-07-07 19:10:53 +0200

    Added .catch() to get request

--- src/components/artist/SimilarArtists.js
+++ src/components/artist/SimilarArtists.js
@@ -7,17 +7,22 @@ export default class SimilarArtists extends Component {
   };
 
   componentDidMount() {
-    console.log(this.props.artist);
+    // console.log(this.props.artist);
     let artist = this.props.artist;
 
     // retrieve similar artists from TasteDive API
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     let url = `https://tastedive.com/api/similar?q=${artist}&type=music&k=377965-Musicall-BCXU1LOW`;
-    axios.get(proxyurl + url).then((res) => {
-      console.log(res);
-      this.setState({ similarArtists: res.data.Similar.Results });
-      console.log(this.state.similarArtists);
-    });
+    axios
+      .get(proxyurl + url)
+      .then((res) => {
+        // console.log(res);
+        this.setState({ similarArtists: res.data.Similar.Results });
+        // console.log(this.state.similarArtists);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
