77e59cca724ba10246a63708de39ecb50a25e4e6
Liam Best <liambbest@googlemail.com>
2020-07-08 20:42:35 +0200

    SpotifyDetails rendering complete

--- src/components/artist/ArtistOverview/SpotifyDetails.js
+++ src/components/artist/ArtistOverview/SpotifyDetails.js
@@ -5,9 +5,30 @@ export default class SpotifyDetails extends Component {
     spotifyDetails: this.props.spotifyDetails,
   };
   componentDidMount() {
-    console.log(this.state.spotifyDetails);
+    // console.log(this.state.spotifyDetails);
   }
   render() {
-    return <div></div>;
+    const spotifyDetails = this.state.spotifyDetails;
+    return (
+      <div>
+        <h1>{spotifyDetails.name}</h1>
+        {spotifyDetails.images[0] ? (
+          <img src={spotifyDetails.images[0].url} alt="artist"></img>
+        ) : (
+          <img
+            src="https://static.thenounproject.com/png/29993-200.png"
+            alt="artist"
+          ></img>
+        )}
+        <a
+          className="spotify-btn"
+          href={spotifyDetails.external_urls.spotify}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open in Spotify
+        </a>
+      </div>
+    );
   }
 }
