084b2802147fdc8179b646340f4507556bb24b60
Liam Best <liambbest@googlemail.com>
2020-07-10 15:59:32 +0200

    Added comments

--- src/components/artist/ArtistOverview/SpotifyDetails.js
+++ src/components/artist/ArtistOverview/SpotifyDetails.js
@@ -4,14 +4,13 @@ export default class SpotifyDetails extends Component {
   state = {
     spotifyDetails: this.props.spotifyDetails,
   };
-  componentDidMount() {
-    // console.log(this.state.spotifyDetails);
-  }
   render() {
     const spotifyDetails = this.state.spotifyDetails;
     return (
       <div>
+        {/*render artist name*/}
         <h1>{spotifyDetails.name}</h1>
+        {/*if there is a Spotify image render it, otherwise use placeholder*/}
         {spotifyDetails.images[0] ? (
           <img src={spotifyDetails.images[0].url} alt="artist"></img>
         ) : (
@@ -20,6 +19,7 @@ export default class SpotifyDetails extends Component {
             alt="artist"
           ></img>
         )}
+        {/*render button which opens artist in Spotify*/}
         <a
           className="spotify-btn"
           href={spotifyDetails.external_urls.spotify}
