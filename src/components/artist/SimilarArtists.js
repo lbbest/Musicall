811f615f032110747a8e4ede33ee68c779a4412b
Liam Best <liambbest@googlemail.com>
2020-07-17 18:21:20 +0200

    Redirect via history push

--- src/components/artist/SimilarArtists.js
+++ src/components/artist/SimilarArtists.js
@@ -37,7 +37,7 @@ export default class SimilarArtists extends Component {
       .get(url, header)
       .then((res) => {
         console.log(res);
-        window.location.pathname = `/artist/${res.data.artists.items[0].id}`;
+        this.props.history.push(`/artist/${res.data.artists.items[0].id}`);
       })
       .catch((err) => {
         console.log(err);
