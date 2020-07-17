3eafcbd900508846759d4204d33c9750e6b92601
Liam Best <liambbest@googlemail.com>
2020-07-17 16:31:11 +0200

    Commented out fake links

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -5,13 +5,13 @@ export default class Nav extends Component {
   render() {
     return (
       <nav>
-        <Link to="/">
+        <Link to="/" className="text-link">
           <h3 id="nav-logo">music.all</h3>
         </Link>
         <ul className="nav-link-container">
-          <li className="nav-link">Featured</li>
+          {/* <li className="nav-link">Featured</li>
           <li className="nav-link">New Releases</li>
-          <li className="nav-link">Blog</li>
+          <li className="nav-link">Blog</li> */}
         </ul>
       </nav>
     );
