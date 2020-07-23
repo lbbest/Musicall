92881c67bbddbf5f7a2df301793512ee5cb98441
Liam Best <liambbest@googlemail.com>
2020-07-23 17:03:29 +0200

    Added releases sort by function

--- src/components/artist/Releases.js
+++ src/components/artist/Releases.js
@@ -5,6 +5,8 @@ import axios from "axios";
 export default class Releases extends Component {
   state = {
     albums: null,
+    // original order held in state to reset sort function
+    originalAlbumOrder: null,
   };
 
   componentDidMount() {
@@ -25,13 +27,57 @@ export default class Releases extends Component {
       .get(url, header)
       .then((res) => {
         // send albums to component state
-        this.setState({ albums: res.data.albums.items });
-        // console.log(this.state.albums);
+        this.setState({
+          albums: res.data.albums.items,
+          originalAlbumOrder: res.data.albums.items,
+        });
+        console.log(this.state.albums);
       })
       .catch((err) => {
         console.log(err);
       });
   }
+
+  // function to sort releases
+  sortReleases = (value) => {
+    switch (value) {
+      case "date":
+        // sort by release date
+        const date = [...this.state.albums].sort((a, b) => {
+          if (a.release_date > b.release_date) return -1;
+          if (a.release_date < b.release_date) return 1;
+          return 0;
+        });
+        this.setState({ albums: date });
+        break;
+      case "title-az":
+        // sort by title a-z
+        const titleaz = [...this.state.albums].sort((a, b) => {
+          if (a.name < b.name) return -1;
+          if (a.name > b.name) return 1;
+          return 0;
+        });
+        this.setState({ albums: titleaz });
+        break;
+      case "title-za":
+        // sort by title z-a
+        const titleza = [...this.state.albums].sort((a, b) => {
+          if (a.name > b.name) return -1;
+          if (a.name < b.name) return 1;
+          return 0;
+        });
+        this.setState({ albums: titleza });
+        break;
+      case "popularity":
+        // sort by popularity
+        this.setState({ albums: this.state.originalAlbumOrder });
+        break;
+      default:
+        // sort by popularity/default order from API
+        this.setState({ albums: this.state.originalAlbumOrder });
+    }
+  };
+
   render() {
     // if albums are present render, otherwise show loader
     const albums = this.state.albums ? (
@@ -83,7 +129,22 @@ export default class Releases extends Component {
     );
     return (
       <div className="artist-section-container">
-        <h3 className="releases-title">Releases:</h3>
+        <div className="releases-header">
+          <h3 className="releases-title">Releases:</h3>
+          {/*Dropdown menu for release sorting*/}
+          <div className="releases-dropdown">
+            <label htmlFor="release-sort">Sort by:</label>
+            <select
+              id="release-sort"
+              onChange={(event) => this.sortReleases(event.target.value)}
+            >
+              <option value="popularity">Popularity</option>
+              <option value="date">Date</option>
+              <option value="title-az">Title (A-Z)</option>
+              <option value="title-za">Title (Z-A)</option>
+            </select>
+          </div>
+        </div>
         {albums}
       </div>
     );
